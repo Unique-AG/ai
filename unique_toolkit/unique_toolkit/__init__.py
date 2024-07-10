e4705aee7d11b153aa2effbaabc6a000238290e3
Sadique Sheik <sadique@unique.ch>
2024-07-10 13:47:19 +0200

    added test pipeline

--- unique_toolkit/unique_toolkit/__init__.py
+++
@@ -1,3 +0,0 @@
-from .observability.log_config import load_logging_config  # noqa: F401
-from .sdk.init import init_sdk  # noqa: F401
-from .security.verify import verify_signature_and_construct_event  # noqa: F401
