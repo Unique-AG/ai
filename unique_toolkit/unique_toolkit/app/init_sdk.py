1757440341f1ffc45b3fab35ce71f8d8e3740e17
Sadique Sheik <sadique@unique.ch>
2024-07-24 15:30:28 +0200

    init_sdk updated to be able to use default values

--- unique_toolkit/unique_toolkit/app/init_sdk.py
+++ unique_toolkit/unique_toolkit/app/init_sdk.py
@@ -3,20 +3,39 @@ import os
 import unique_sdk
 
 
-def init_sdk() -> str:
-    """Initializes the SDK."""
-    unique_sdk.api_key = get_env("API_KEY")
-    unique_sdk.app_id = get_env("APP_ID")
-    api_base = os.environ.get("API_BASE")
-    if api_base:
-        unique_sdk.api_base = api_base
+def get_env(var_name, default=None, strict=False):
+    """Get the environment variable.
 
-    endpoint_secret = get_env("ENDPOINT_SECRET")
-    return endpoint_secret
+    Args:
+        var_name (str): Name of the environment variable.
+        default (str, optional): Default value. Defaults to None.
+        strict (bool, optional): This method raises a ValueError, if strict, and no value is found in the environment. Defaults to False.
 
+    Raises:
+        ValueError: _description_
 
-def get_env(var_name):
+    Returns:
+        _type_: _description_
+    """
     val = os.environ.get(var_name)
     if not val:
-        raise ValueError(f"{var_name} is not set")
-    return val
+        if strict:
+            raise ValueError(f"{var_name} is not set")
+    return val or default
+
+
+def init_sdk(strict_all_vars=False):
+    """Initialize the SDK.
+
+    Args:
+        strict_all_vars (bool, optional): This method raises a ValueError if strict and no value is found in the environment. Defaults to False.
+    """
+    unique_sdk.api_key = get_env("API_KEY", default="dummy", strict=strict_all_vars)
+    unique_sdk.app_id = get_env("APP_ID", default="dummy", strict=strict_all_vars)
+    unique_sdk.api_base = get_env("API_BASE", default=None, strict=strict_all_vars)
+
+
+def get_endpoint_secret():
+    """Fetch endpoint secret from the environment."""
+    endpoint_secret = os.getenv("ENDPOINT_SECRET")
+    return endpoint_secret
