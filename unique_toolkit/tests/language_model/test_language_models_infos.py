8437117d60c7a20b21876da6d05ac234824eaf44
mfadler <martin.fadler@unique.ch>
2024-08-26 17:09:15 +0200

    add test

--- unique_toolkit/tests/language_model/test_language_models_infos.py
+++ unique_toolkit/tests/language_model/test_language_models_infos.py
@@ -11,7 +11,7 @@ from unique_toolkit.language_model.infos import (
 class TestLanguageModelInfos:
     def test_can_list_all_models(self):
         models = LanguageModel.list_models()
-        assert len(models) == 9
+        assert len(models) == 10
         assert all(isinstance(model, LanguageModelInfo) for model in models)
         assert all(model.name for model in models)
 
@@ -25,6 +25,7 @@ class TestLanguageModelInfos:
         assert LanguageModelName.AZURE_GPT_35_TURBO in model_names
         assert LanguageModelName.AZURE_GPT_35_TURBO_0613 in model_names
         assert LanguageModelName.AZURE_GPT_4o_2024_0513 in model_names
+        assert LanguageModelName.AZURE_GPT_4o_MINI_2024_0718 in model_names
 
     def test_get_language_model(self):
         model = LanguageModel(LanguageModelName.AZURE_GPT_4_TURBO_1106)
