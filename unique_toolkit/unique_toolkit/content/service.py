3a3eb2d952baa9bf3644750581b3e09f1bbc0fe2
Sadique Sheik <sadique@unique.ch>
2024-08-30 10:25:43 +0200

    added metadata filter to function params

--- unique_toolkit/unique_toolkit/content/service.py
+++ unique_toolkit/unique_toolkit/content/service.py
@@ -53,7 +53,7 @@ class ContentService(BaseService):
             reranker_config (Optional[ContentRerankerConfig]): The reranker configuration. Defaults to None.
             scope_ids (Optional[list[str]]): The scope IDs. Defaults to None.
             chat_only (Optional[bool]): Whether to search only in the current chat. Defaults to None.
-            metadata_filter (Optional[dict]): The metadata filter. Defaults to None.
+            metadata_filter (Optional[dict]): UniqueQL metadata filter. Defaults to None.
 
         Returns:
             list[ContentChunk]: The search results.
@@ -112,7 +112,7 @@ class ContentService(BaseService):
             reranker_config (Optional[ContentRerankerConfig]): The reranker configuration. Defaults to None.
             scope_ids (Optional[list[str]]): The scope IDs. Defaults to None.
             chat_only (Optional[bool]): Whether to search only in the current chat. Defaults to None.
-            metadata_filter (Optional[dict]): The metadata filter. Defaults to None.
+            metadata_filter (Optional[dict]): UniqueQL metadata filter. Defaults to None.
 
         Returns:
             list[ContentChunk]: The search results.
