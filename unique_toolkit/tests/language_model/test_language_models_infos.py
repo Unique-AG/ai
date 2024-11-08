f06495907ccfd28528194f9f75f25d89b443d942
Cedric Klinkert <cedric@unique.ch>
2024-11-08 05:24:29 +0100

    Replace subclassing pattern by dict

--- unique_toolkit/tests/language_model/test_language_models_infos.py
+++ unique_toolkit/tests/language_model/test_language_models_infos.py
@@ -4,16 +4,16 @@ import pytest
 
 from unique_toolkit.language_model.infos import (
     EncoderName,
-    LanguageModel,
     LanguageModelInfo,
     LanguageModelName,
     LanguageModelProvider,
+    LanguageModelRegistry,
 )
 
 
 class TestLanguageModelInfos:
     def test_can_list_all_defined_models(self):
-        models = LanguageModel.list_models()
+        models = LanguageModelRegistry.list_models()
         assert len(models) == 11
         assert all(isinstance(model, LanguageModelInfo) for model in models)
         assert all(model.name for model in models)
@@ -32,7 +32,7 @@ class TestLanguageModelInfos:
         assert LanguageModelName.AZURE_GPT_4o_MINI_2024_0718 in model_names
 
     def test_get_language_model(self):
-        model = LanguageModel(LanguageModelName.AZURE_GPT_4_TURBO_1106)
+        model = LanguageModelRegistry.get_model_info(LanguageModelName.AZURE_GPT_4_TURBO_1106)
 
         assert model.name == LanguageModelName.AZURE_GPT_4_TURBO_1106
         assert model.provider == LanguageModelProvider.AZURE
@@ -45,7 +45,7 @@ class TestLanguageModelInfos:
         assert model.encoder_name == EncoderName.CL100K_BASE
 
     def test_get_custom_language_model(self):
-        model = LanguageModel("My Custom Model")
+        model = LanguageModelRegistry.get_model_info("My Custom Model")
 
         assert model.name == "My Custom Model"
         assert model.provider == LanguageModelProvider.CUSTOM
@@ -61,5 +61,5 @@ class TestLanguageModelInfos:
         assert model.retirement_text is None
 
     def test_get_language_model_raises_error_for_invalid_model(self):
-        with pytest.raises(ValueError):
-            LanguageModel("")
+        with pytest.raises(KeyError):
+            LanguageModelRegistry.get_model_info("")
--- unique_toolkit/unique_toolkit/language_model/infos.py
+++ unique_toolkit/unique_toolkit/language_model/infos.py
@@ -74,203 +74,105 @@ class LanguageModelInfo(BaseModel):
     deprecated_at: Optional[date] = None
     retirement_text: Optional[str] = None
 
-
-class LanguageModel:
-    _info: ClassVar[LanguageModelInfo]
-
-    def __init__(self, model_name: LanguageModelName | str):
-        self._model_info = self.get_model_info(model_name)
-
-    @property
-    def info(self) -> LanguageModelInfo:
-        """
-        Returns all infos about the model:
-        - name
-        - version
-        - provider
-        - encoder_name
-        - token_limits
-        - info_cutoff_at
-        - published_at
-        - retirement_at
-        - deprecated_at
-        - retirement_text
-        """
-        return self._model_info
-
-    @property
-    def name(self) -> LanguageModelName | str:
-        """
-        Returns the LanguageModelName of the model or the name string when it is a custom / not defined model.
-        """
-        return self._model_info.name
-
     @property
     def display_name(self) -> str:
         """
         Returns the name of the model as a string.
         """
-        if isinstance(self._model_info.name, LanguageModelName):
-            return self._model_info.name.name
+        if isinstance(self.name, LanguageModelName):
+            return self.name.name
         else:
-            return self._model_info.name
-
-    @property
-    def version(self) -> Optional[str]:
-        """
-        Returns the version of the model.
-        """
-        return self._model_info.version
-
-    @property
-    def encoder_name(self) -> Optional[EncoderName]:
-        """
-        Returns the encoder_name used for the model.
-        """
-        return self._model_info.encoder_name
+            return self.name
 
     @property
     def token_limit(self) -> Optional[int]:
         """
         Returns the maximum number of tokens for the model.
         """
-        if self._model_info.token_limits:
-            return self._model_info.token_limits.token_limit
+        if self.token_limits:
+            return self.token_limits.token_limit
 
     @property
     def token_limit_input(self) -> Optional[int]:
         """
         Returns the maximum number of input tokens for the model.
         """
-        if self._model_info.token_limits:
-            return self._model_info.token_limits.token_limit_input
+        if self.token_limits:
+            return self.token_limits.token_limit_input
 
     @property
     def token_limit_output(self) -> Optional[int]:
         """
         Returns the maximum number of output tokens for the model.
         """
-        if self._model_info.token_limits:
-            return self._model_info.token_limits.token_limit_output
+        if self.token_limits:
+            return self.token_limits.token_limit_output
 
-    @property
-    def info_cutoff_at(self) -> Optional[date]:
-        """
-        Returns the date the model was last updated.
-        """
-        return self._model_info.info_cutoff_at
 
-    @property
-    def published_at(self) -> Optional[date]:
-        """
-        Returns the date the model was published.
-        """
-        return self._model_info.published_at
+class LanguageModelRegistry:
+    models: dict[LanguageModelName, LanguageModelInfo] = {}
 
-    @property
-    def retirement_at(self) -> Optional[date]:
-        """
-        Returns the date the model will be retired.
-        """
-        return self._model_info.retirement_at
-
-    @property
-    def deprecated_at(self) -> Optional[date]:
-        """
-        Returns the date the model was deprecated.
-        """
-        return self._model_info.deprecated_at
-
-    @property
-    def retirement_text(self) -> Optional[str]:
-        """
-        Returns the text that will be displayed when the model is retired.
-        """
-        return self._model_info.retirement_text
-
-    @property
-    def provider(self) -> LanguageModelProvider:
-        """
-        Returns the provider of the model.
-        """
-        return self._model_info.provider
-
-    @classmethod
-    def get_model_info(cls, model_name: LanguageModelName | str) -> LanguageModelInfo:
-        if not model_name:
-            raise ValueError("Model name must be provided to get the model info.")
-
-        for subclass in cls.__subclasses__():
-            if hasattr(subclass, "info") and subclass._info.name == model_name:
-                # TODO find alternative solution for warning
-                # if subclass._info.retirement_at:
-                #     warning_text = f"WARNING: {subclass._info.name} will be retired on {subclass._info.retirement_at.isoformat()} and from then on not accessible anymore. {subclass._info.retirement_text}"
-                #     print(warning_text)
-                #     warnings.warn(warning_text, DeprecationWarning, stacklevel=2)
-                return subclass._info
-
-        return LanguageModelInfo(
-            name=model_name,
-            version="custom",
-            provider=LanguageModelProvider.CUSTOM,
+    @staticmethod
+    def register_language_model(
+        name: LanguageModelName,
+        version: str,
+        provider: LanguageModelProvider,
+        info_cutoff_at: date,
+        published_at: date,
+        encoder_name: Optional[EncoderName] = None,
+        token_limit: Optional[int] = None,
+        token_limit_input: Optional[int] = None,
+        token_limit_output: Optional[int] = None,
+        retirement_at: Optional[date] = None,
+        deprecated_at: Optional[date] = None,
+        retirement_text: Optional[str] = None,
+    ) -> LanguageModelInfo:
+        info = LanguageModelInfo(
+            name=name,
+            version=version,
+            provider=provider,
+            encoder_name=encoder_name,
+            token_limits=LanguageModelTokenLimits(
+                token_limit=token_limit,
+                token_limit_input=token_limit_input,
+                token_limit_output=token_limit_output,
+            ),
+            info_cutoff_at=info_cutoff_at,
+            published_at=published_at,
+            retirement_at=retirement_at,
+            deprecated_at=deprecated_at,
+            retirement_text=retirement_text,
         )
+        LanguageModel.models[name] = info
 
-    @classmethod
-    def list_models(cls) -> list[LanguageModelInfo]:
+    @staticmethod
+    def get_model_info(name: LanguageModelName | str) -> LanguageModelInfo:
+        return LanguageModel.models[name]
+    
+    @staticmethod
+    def list_models() -> list[LanguageModelInfo]:
         """
         Returns a list of the infos of all available models.
         """
 
         return [
-            cast(LanguageModelInfo, subclass._info)
-            for subclass in cls.__subclasses__()
-            if hasattr(subclass, "_info")
+            cast(LanguageModelInfo, modelinfo)
+            for modelinfo in LanguageModel.models.values()
+            if hasattr(modelinfo, "name")
         ]
 
 
-def create_language_model(
-    name: LanguageModelName,
-    version: str,
-    provider: LanguageModelProvider,
-    info_cutoff_at: date,
-    published_at: date,
-    encoder_name: Optional[EncoderName] = None,
-    token_limit: Optional[int] = None,
-    token_limit_input: Optional[int] = None,
-    token_limit_output: Optional[int] = None,
-    retirement_at: Optional[date] = None,
-    deprecated_at: Optional[date] = None,
-    retirement_text: Optional[str] = None,
-) -> Type[LanguageModel]:
-    info = LanguageModelInfo(
-        name=name,
-        version=version,
-        provider=provider,
-        encoder_name=encoder_name,
-        token_limits=LanguageModelTokenLimits(
-            token_limit=token_limit,
-            token_limit_input=token_limit_input,
-            token_limit_output=token_limit_output,
-        ),
-        info_cutoff_at=info_cutoff_at,
-        published_at=published_at,
-        retirement_at=retirement_at,
-        deprecated_at=deprecated_at,
-        retirement_text=retirement_text,
-    )
 
-    class Model(LanguageModel):
-        _info = info
-
-    return Model
-
+# Alias to deprecate
+LanguageModel = LanguageModelRegistry
 
 ############################################################################################################
 # Define the models here
 ############################################################################################################
 
+register_language_model = LanguageModelRegistry.register_language_model
 
-AzureGpt35Turbo0613 = create_language_model(
+AzureGpt35Turbo0613 = register_language_model(
     name=LanguageModelName.AZURE_GPT_35_TURBO_0613,
     provider=LanguageModelProvider.AZURE,
     version="0613",
@@ -281,7 +183,7 @@ AzureGpt35Turbo0613 = create_language_model(
     retirement_at=date(2024, 10, 1),
 )
 
-AzureGpt35Turbo = create_language_model(
+AzureGpt35Turbo = register_language_model(
     name=LanguageModelName.AZURE_GPT_35_TURBO,
     provider=LanguageModelProvider.AZURE,
     version="0301",
@@ -291,8 +193,7 @@ AzureGpt35Turbo = create_language_model(
     published_at=date(2023, 3, 1),
 )
 
-
-AzureGpt35Turbo16k = create_language_model(
+AzureGpt35Turbo16k = register_language_model(
     name=LanguageModelName.AZURE_GPT_35_TURBO_16K,
     provider=LanguageModelProvider.AZURE,
     version="0613",
@@ -303,8 +204,7 @@ AzureGpt35Turbo16k = create_language_model(
     retirement_at=date(2024, 10, 1),
 )
 
-
-AzureGpt40613 = create_language_model(
+AzureGpt40613 = register_language_model(
     name=LanguageModelName.AZURE_GPT_4_0613,
     provider=LanguageModelProvider.AZURE,
     version="0613",
@@ -316,8 +216,7 @@ AzureGpt40613 = create_language_model(
     retirement_at=date(2025, 6, 1),
 )
 
-
-AzureGpt4Turbo1106 = create_language_model(
+AzureGpt4Turbo1106 = register_language_model(
     name=LanguageModelName.AZURE_GPT_4_TURBO_1106,
     provider=LanguageModelProvider.AZURE,
     version="1106-preview",
@@ -328,8 +227,7 @@ AzureGpt4Turbo1106 = create_language_model(
     published_at=date(2023, 11, 6),
 )
 
-
-AzureGpt4VisionPreview = create_language_model(
+AzureGpt4VisionPreview = register_language_model(
     name=LanguageModelName.AZURE_GPT_4_VISION_PREVIEW,
     provider=LanguageModelProvider.AZURE,
     version="vision-preview",
@@ -340,7 +238,7 @@ AzureGpt4VisionPreview = create_language_model(
     published_at=date(2023, 11, 6),
 )
 
-AzureGpt432k0613 = create_language_model(
+AzureGpt432k0613 = register_language_model(
     name=LanguageModelName.AZURE_GPT_4_32K_0613,
     provider=LanguageModelProvider.AZURE,
     version="1106-preview",
@@ -352,7 +250,7 @@ AzureGpt432k0613 = create_language_model(
     retirement_at=date(2025, 6, 1),
 )
 
-AzureGpt4Turbo20240409 = create_language_model(
+AzureGpt4Turbo20240409 = register_language_model(
     name=LanguageModelName.AZURE_GPT_4_TURBO_2024_0409,
     encoder_name=get_encoder_name(LanguageModelName.AZURE_GPT_4_TURBO_2024_0409),
     provider=LanguageModelProvider.AZURE,
@@ -363,7 +261,7 @@ AzureGpt4Turbo20240409 = create_language_model(
     published_at=date(2024, 4, 9),
 )
 
-AzureGpt4o20240513 = create_language_model(
+AzureGpt4o20240513 = register_language_model(
     name=LanguageModelName.AZURE_GPT_4o_2024_0513,
     encoder_name=get_encoder_name(LanguageModelName.AZURE_GPT_4o_2024_0513),
     provider=LanguageModelProvider.AZURE,
@@ -374,7 +272,7 @@ AzureGpt4o20240513 = create_language_model(
     published_at=date(2024, 5, 13),
 )
 
-AzureGpt4o20240806 = create_language_model(
+AzureGpt4o20240806 = register_language_model(
     name=LanguageModelName.AZURE_GPT_4o_2024_0806,
     encoder_name=get_encoder_name(LanguageModelName.AZURE_GPT_4o_2024_0806),
     provider=LanguageModelProvider.AZURE,
@@ -385,7 +283,7 @@ AzureGpt4o20240806 = create_language_model(
     published_at=date(2024, 8, 6),
 )
 
-AzureGpt4oMini20240718 = create_language_model(
+AzureGpt4oMini20240718 = register_language_model(
     name=LanguageModelName.AZURE_GPT_4o_MINI_2024_0718,
     provider=LanguageModelProvider.AZURE,
     version="2024-07-18",
