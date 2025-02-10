7f523aae091f700bfc4d4b6008490cc35352a20d
Sadique Sheik <sadique@unique.ch>
2025-02-10 16:42:10 +0100

    included missing attributes to ChatMessage

--- unique_toolkit/unique_toolkit/chat/schemas.py
+++ unique_toolkit/unique_toolkit/chat/schemas.py
@@ -42,12 +42,18 @@ class ChatMessage(BaseModel):
     model_config = model_config
 
     id: str | None = None
+    chat_id: str
     object: str | None = None
     content: str = Field(alias="text")
+    original_content: str | None = Field(alias="originalText")
     role: ChatMessageRole
+    gptRequest: list[dict] | None = None
     tool_calls: Optional[list[ToolCall]] = None
     tool_call_id: Optional[str] = None
     debug_info: dict | None = {}
+    created_at: datetime
+    completed_at: datetime | None = None
+    updated_at: datetime
 
     # TODO make sdk return role consistently in lowercase
     # Currently needed as sdk returns role in uppercase
