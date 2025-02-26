9ff015f20d77e453469b3a9fa447028c0debd034
Cedric Klinkert <cedric@unique.ch>
2025-02-26 19:25:06 +0100

    feat(language_model): Add image message support and improve type hints
    - Added `image_message_append` method to `MessagesBuilder` for supporting image messages
    - Enhanced type hints for message builder methods using `typing_extensions.Self`
    - Added `builder()` and `create_builder()` methods to `LanguageModelMessages` for easier message sequence creation

--- unique_toolkit/unique_toolkit/language_model/builder.py
+++ unique_toolkit/unique_toolkit/language_model/builder.py
@@ -1,3 +1,5 @@
+from typing_extensions import Self
+
 from unique_toolkit.language_model import (
     LanguageModelAssistantMessage,
     LanguageModelMessage,
@@ -12,25 +14,38 @@ class MessagesBuilder:
     def __init__(self):
         self.messages: list[LanguageModelMessage] = []
 
-    def system_message_append(self, content):
-        """Appends a user message to the messages list."""
+    def system_message_append(self, content: str) -> Self:
+        """Appends a system message to the messages list."""
         message = LanguageModelSystemMessage(content=content)
         self.messages.append(message)
         return self  # Return self to allow method chaining
 
-    def user_message_append(self, content):
+    def user_message_append(self, content: str) -> Self:
         """Appends a user message to the messages list."""
         message = LanguageModelUserMessage(content=content)
         self.messages.append(message)
         return self  # Return self to allow method chaining
 
-    def assistant_message_append(self, content):
+    def image_message_append(self, content: str, images: list[str]) -> Self:
+        message = LanguageModelUserMessage(
+            content=[
+                {"type": "text", "text": content},
+                *[
+                    {"type": "image_url", "imageUrl": {"url": image}}
+                    for image in images
+                ],
+            ])
+        self.messages.append(message)
+        return self
+
+
+    def assistant_message_append(self, content: str) -> Self:
         """Appends an assistant message to the messages list."""
         message = LanguageModelAssistantMessage(content=content)
         self.messages.append(message)
         return self  # Return self to allow method chaining
 
-    def tool_message_append(self, name: str, tool_call_id: str, content: str):
+    def tool_message_append(self, name: str, tool_call_id: str, content: str) -> Self:
         """Appends a tool message to the messages list."""
         message = LanguageModelToolMessage(
             name=name, tool_call_id=tool_call_id, content=content
