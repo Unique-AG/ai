e52a01f1e98f8090a7651a46d669e6add987f900
Abiodun <abiodun.shuaib.m@gmail.com>
2025-03-14 02:22:15 +0100

    unqiue integration testing with files

--- unique_sdk/tests/api_resources/test_integration_content.py
+++ unique_sdk/tests/api_resources/test_integration_content.py
@@ -53,9 +53,8 @@ class TestContent:
                 "title": "Test Document",
                 "mimeType": "text/plain",
                 "ownerType": "user",
-                "ownerId": event.user_id,
                 "byteSize": 100,
-                "ingestionConfig": {"uniqueIngestionMode": "default"},
+                "ingestionConfig": {"uniqueIngestionMode": "SKIP_INGESTION"},
             },
         )
 
@@ -66,6 +65,35 @@ class TestContent:
             chatId=event.chat_id,
             sourceOwnerType="user",
             storeInternally=True,
+        )
+
+        assert isinstance(response.id, str)
+        assert isinstance(response.key, str)
+        assert response.key == "test-document"
+    
+    @pytest.mark.unit
+    def test_upsert_content_file_upload(self, event):
+        """Test upserting content synchronously in sandbox."""
+        input_data = cast(
+            Content.Input,
+            {
+                "key": "test-document",
+                "title": "Test Document 2",
+                "mimeType": "text/plain",
+                "ownerType": "user",
+                "byteSize": 100,
+                "ingestionConfig": {"uniqueIngestionMode": "SKIP_INGESTION"},
+            },
+        )
+
+        response = Content.upsert(
+            user_id=event.user_id,
+            company_id=event.company_id,
+            input=input_data,
+            chatId=event.chat_id,
+            sourceOwnerType="user",
+            storeInternally=True,
+            fileUrl="some_texts.txt"
         )
 
         assert isinstance(response.id, str)
