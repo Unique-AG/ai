750675d9c3b8e7c03f1e279772225c510e86943a
Abiodun <abiodun0@users.noreply.github.com>
2025-04-04 11:31:20 +0100

    chore(ai-module): add gpt-45-configuration (#145)

--- unique_toolkit/CHANGELOG.md
+++ unique_toolkit/CHANGELOG.md
@@ -5,6 +5,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), 
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.7.4] - 2025-03-20
+- add `AZURE_GPT_45_PREVIEW_2025_0227` as part of the models
 
 ## [0.7.3] - 2025-03-20
 - Enable handling tool calls in message builder
--- unique_toolkit/unique_toolkit/language_model/infos.py
+++ unique_toolkit/unique_toolkit/language_model/infos.py
@@ -20,6 +20,7 @@ class LanguageModelName(StrEnum):
     AZURE_o1_2024_1217 = "AZURE_o1_2024_1217"
     AZURE_o1_MINI_2024_0912 = "AZURE_o1_MINI_2024_0912"
     AZURE_o3_MINI_2025_0131 = "AZURE_o3_MINI_2025_0131"
+    AZURE_GPT_45_PREVIEW_2025_0227 = "AZURE_GPT_45_PREVIEW_2025_0227"
 
 
 class EncoderName(StrEnum):
@@ -274,6 +275,25 @@ class LanguageModelInfo(BaseModel):
                     ),
                     info_cutoff_at=date(2023, 10, 1),
                     published_at=date(2025, 1, 31),
+                )
+            case LanguageModelName.AZURE_GPT_45_PREVIEW_2025_0227:
+                return cls(
+                    name=model_name,
+                    capabilities=[
+                        ModelCapabilities.STRUCTURED_OUTPUT,
+                        ModelCapabilities.FUNCTION_CALLING,
+                        ModelCapabilities.STREAMING,
+                        ModelCapabilities.REASONING,
+                        ModelCapabilities.VISION,
+                    ],
+                    provider=LanguageModelProvider.AZURE,
+                    version="2025-02-27",
+                    encoder_name=EncoderName.O200K_BASE,
+                    token_limits=LanguageModelTokenLimits(
+                        token_limit_input=128_000, token_limit_output=16_384
+                    ),
+                    info_cutoff_at=date(2023, 10, 1),
+                    published_at=date(2025, 2, 27),
                 )
             case _:
                 if isinstance(model_name, LanguageModelName):
