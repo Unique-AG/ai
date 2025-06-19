df2b1e32ba3052375f1e1a4b9bb962e3fe8da4a8
Cedric Klinkert <cedric@unique.ch>
2025-06-19 17:23:31 +0200

    fix: loading of event with null for raw scope rule

--- tutorials/unique_basics/toolkit_examples/testable_llm_service.py
+++ tutorials/unique_basics/toolkit_examples/testable_llm_service.py
@@ -4,6 +4,8 @@ The language model service can be used for testing and the
 chat services can directly be used for the unique frontend.
 """
 
+# %%
+
 from pathlib import Path
 
 from utilities_examples.init_sdk import init_from_env_file
@@ -21,17 +23,34 @@ class MyService:
     def run(self, messages: LanguageModelMessages):
         # Here you would actually create your messages
 
-        self._rag_service.complete_with_references(
+        response = self._rag_service.complete_with_references(
             messages=messages,
-            model_name=LanguageModelName.AZURE_GPT_4_0613,
+            model_name=LanguageModelName.AZURE_GPT_4o_2024_0806,
         )
+
+        print(response.message.text)
 
 
 company_id, user_id = init_from_env_file(Path(__file__).parent / ".." / ".env")
 
-event = Event.from_json_file(Path("/path/to/event.json"))
 llm_service = LanguageModelService(company_id=company_id, user_id=user_id)
+
+# You need to create you own event.json in the folder containing this file
+event = Event.from_json_file(Path(__file__).parent / "event.json")
 chat_service = ChatService(event=event)
 
+
+messages = (
+    LanguageModelMessages([])
+    .builder()
+    .system_message_append("You are a helpful assistant")
+    .user_message_append("What is the capital of France?")
+    .build()
+)
+
 my_test_service = MyService(rag_service=llm_service)
+my_test_service.run(messages=messages)
+
 my_chat_service = MyService(rag_service=chat_service)
+my_chat_service.run(messages=messages)
+# %%
