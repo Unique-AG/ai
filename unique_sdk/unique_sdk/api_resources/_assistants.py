03e1fd989649d74a98f3c64f796798aa0a033767
irina-unique <irina@unique.ch>
2025-07-07 23:19:08 +0200

    feat: add full support for assistants api

--- unique_sdk/unique_sdk/api_resources/_assistants.py
+++ unique_sdk/unique_sdk/api_resources/_assistants.py
@@ -1,7 +1,10 @@
 from typing import (
+    Any,
     ClassVar,
+    List,
     Literal,
     NotRequired,
+    TypedDict,
     Unpack,
     cast,
 )
@@ -14,9 +17,41 @@ class Assistants(APIResource["Assistants"]):
     OBJECT_NAME: ClassVar[Literal["openai.assistant"]] = "openai.assistant"
 
     class CreateParams(RequestOptions):
-        # expand the input type to allow more complex structures. e.g.: image or file inputs
         name: str
         instructions: str
+        model: NotRequired[
+            Literal[
+                "AZURE_o4_MINI_2025_0416",
+                "AZURE_o3_2025_0416",
+            ]
+        ]
+
+    class CreateThreadParams(RequestOptions):
+        messages: List[Any]
+
+    class Thread(TypedDict):
+        id: str
+
+    class CreateMessageParams(RequestOptions):
+        content: Any
+        role: Literal["user", "assistant"]
+
+    class Message(TypedDict):
+        id: str
+        content: Any
+        role: Literal["user", "assistant"]
+
+    class CreateRunParams(RequestOptions):
+        assistant_id: str
+        model: NotRequired[
+            Literal[
+                "AZURE_o4_MINI_2025_0416",
+                "AZURE_o3_2025_0416",
+            ]
+        ]
+
+    class Run(TypedDict):
+        id: str
         model: NotRequired[
             Literal[
                 "AZURE_o4_MINI_2025_0416",
@@ -28,14 +63,14 @@ class Assistants(APIResource["Assistants"]):
     def create(
         cls,
         company_id: str,
-        user_id: str | None = None,
+        user_id: str,
         **params: Unpack["Assistants.CreateParams"],
     ) -> "Assistants":
         return cast(
             Assistants,
             cls._static_request(
                 "post",
-                "/openai/assistant",
+                "/openai/assistants",
                 company_id=company_id,
                 user_id=user_id,
                 params=params,
@@ -46,14 +81,126 @@ class Assistants(APIResource["Assistants"]):
     async def create_async(
         cls,
         company_id: str,
-        user_id: str | None = None,
+        user_id: str,
         **params: Unpack["Assistants.CreateParams"],
     ) -> "Assistants":
         return cast(
             Assistants,
             await cls._static_request_async(
                 "post",
-                "/openai/assistant",
+                "/openai/assistants",
+                company_id=company_id,
+                user_id=user_id,
+                params=params,
+            ),
+        )
+
+    @classmethod
+    def create_thread(
+        cls,
+        company_id: str,
+        user_id: str,
+        **params: Unpack["Assistants.CreateThreadParams"],
+    ) -> "Assistants.Thread":
+        return cast(
+            Assistants.Thread,
+            cls._static_request(
+                "post",
+                "/openai/threads",
+                company_id=company_id,
+                user_id=user_id,
+                params=params,
+            ),
+        )
+
+    @classmethod
+    async def create_thread_async(
+        cls,
+        company_id: str,
+        user_id: str,
+        **params: Unpack["Assistants.CreateThreadParams"],
+    ) -> "Assistants.Thread":
+        return cast(
+            Assistants.Thread,
+            await cls._static_request_async(
+                "post",
+                "/openai/threads",
+                company_id=company_id,
+                user_id=user_id,
+                params=params,
+            ),
+        )
+
+    @classmethod
+    def create_message(
+        cls,
+        company_id: str,
+        user_id: str,
+        thread_id: str,
+        **params: Unpack["Assistants.CreateMessageParams"],
+    ) -> "Assistants.Message":
+        return cast(
+            Assistants.Message,
+            cls._static_request(
+                "post",
+                f"/openai/threads/{thread_id}/messages",
+                company_id=company_id,
+                user_id=user_id,
+                params=params,
+            ),
+        )
+
+    @classmethod
+    async def create_message_async(
+        cls,
+        company_id: str,
+        user_id: str,
+        thread_id: str,
+        **params: Unpack["Assistants.CreateMessageParams"],
+    ) -> "Assistants.Message":
+        return cast(
+            Assistants.Message,
+            await cls._static_request_async(
+                "post",
+                f"/openai/threads/{thread_id}/messages",
+                company_id=company_id,
+                user_id=user_id,
+                params=params,
+            ),
+        )
+
+    @classmethod
+    def create_run(
+        cls,
+        company_id: str,
+        user_id: str,
+        thread_id: str,
+        **params: Unpack["Assistants.CreateRunParams"],
+    ) -> "Assistants.Run":
+        return cast(
+            Assistants.Run,
+            cls._static_request(
+                "post",
+                f"/openai/threads/{thread_id}/runs",
+                company_id=company_id,
+                user_id=user_id,
+                params=params,
+            ),
+        )
+
+    @classmethod
+    async def create_run_async(
+        cls,
+        company_id: str,
+        user_id: str,
+        thread_id: str,
+        **params: Unpack["Assistants.CreateRunParams"],
+    ) -> "Assistants.Run":
+        return cast(
+            Assistants.Run,
+            await cls._static_request_async(
+                "post",
+                f"/openai/threads/{thread_id}/runs",
                 company_id=company_id,
                 user_id=user_id,
                 params=params,
