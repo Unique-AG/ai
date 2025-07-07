36d0ef090189c332c6c587ba53c9b1f023c2e33b
irina-unique <irina@unique.ch>
2025-07-08 00:05:20 +0200

    fix: add code_interpreter support

--- unique_sdk/unique_sdk/api_resources/_assistants.py
+++ unique_sdk/unique_sdk/api_resources/_assistants.py
@@ -4,7 +4,6 @@ from typing import (
     Dict,
     List,
     Literal,
-    NotRequired,
     Optional,
     TypedDict,
     Union,
@@ -28,17 +27,17 @@ class Assistants(APIResource["Assistants"]):
 
     class FileSearchToolOverrides(TypedDict, total=False):
         max_num_results: int | None = None
-        ranking_options: Optional["Assistants.FileSearchRankingOptions"]
+        ranking_options: Optional["Assistants.FileSearchRankingOptions"] = None
 
     class FunctionToolFunction(TypedDict, total=False):
         name: str
         description: str | None = None
-        parameters: Optional[Dict[str, Any]]
+        parameters: Optional[Dict[str, Any]] = None
         strict: bool | None
 
     class FileSearchTool(TypedDict, total=False):
         type: Literal["file_search"]
-        file_search: Optional["Assistants.FileSearchToolOverrides"]
+        file_search: Optional["Assistants.FileSearchToolOverrides"] = None
 
     class FunctionTool(TypedDict):
         type: Literal["function"]
@@ -57,58 +56,95 @@ class Assistants(APIResource["Assistants"]):
         vector_store_ids: List[str]
 
     class ToolResources(TypedDict, total=False):
-        code_interpreter: Optional["Assistants.CodeInterpreterResources"]
-        file_search: Optional["Assistants.FileSearchResources"]
+        code_interpreter: Optional["Assistants.CodeInterpreterResources"] = None
+        file_search: Optional["Assistants.FileSearchResources"] = None
 
     class CreateParams(RequestOptions):
         name: str
         instructions: str
-        model: NotRequired[
-            Literal[
-                "AZURE_o4_MINI_2025_0416",
-                "AZURE_o3_2025_0416",
-            ]
-        ]
+        model: str
         tools: list["Assistants.ToolDefinition"]
         metadata: dict | None = None
         description: str | None = None
         temperature: float | None = None
         top_p: float | None = None
         max_tokens: int | None = None
-        tool_resources: Optional["Assistants.ToolResources"]
+        tool_resources: Optional["Assistants.ToolResources"] = None
 
-    class CreateThreadParams(RequestOptions):
-        messages: List[Any]
+    class CodeInterpreterToolAttachement(TypedDict):
+        type: Literal["code_interpreter"]
 
-    class Thread(TypedDict):
-        id: str
+    class FileSearchToolAttachement(TypedDict):
+        type: Literal["file_search"]
+
+    AttachmentTool = Union[CodeInterpreterToolAttachement, FileSearchToolAttachement]
+
+    class Attachment(TypedDict, total=False):
+        file_id: str
+        tools: List["Assistants.AttachmentTool"]
 
     class CreateMessageParams(RequestOptions):
         content: Any
         role: Literal["user", "assistant"]
+        attachments: List["Assistants.Attachment"] | None = None
+        metadata: dict | None = None
+
+    class CreateThreadParams(RequestOptions):
+        messages: List["Assistants.CreateMessageParams"]
+        metadata: dict | None = None
+        tool_resources: Optional["Assistants.ToolResources"] = None
+
+    class Thread(TypedDict):
+        id: str
+        tool_resources: Optional["Assistants.ToolResources"] = None
 
     class Message(TypedDict):
         id: str
         content: Any
         role: Literal["user", "assistant"]
+        thread_id: str
+        assistant_id: str
+        attachments: List["Assistants.Attachment"] | None = None
+        status: Literal["in_progress", "incomplete", "completed"]
 
     class CreateRunParams(RequestOptions):
         assistant_id: str
-        model: NotRequired[
-            Literal[
-                "AZURE_o4_MINI_2025_0416",
-                "AZURE_o3_2025_0416",
-            ]
-        ]
+        model: str
+
+    class ToolChoiceFileSearch(TypedDict):
+        type: Literal["file_search"]
+
+    class ToolChoiceFunction(TypedDict):
+        type: Literal["function"]
+        function: Dict[str, Any]
+
+    ToolChoiceObject = Union[ToolChoiceFileSearch, ToolChoiceFunction]
+
+    ToolChoice = Union[
+        Literal["none", "auto", "required"],
+        ToolChoiceObject,
+    ]
 
     class Run(TypedDict):
         id: str
-        model: NotRequired[
-            Literal[
-                "AZURE_o4_MINI_2025_0416",
-                "AZURE_o3_2025_0416",
-            ]
-        ]
+        assistant_id: str
+        thread_id: str
+        status: str
+        model: str
+        tools: List["Assistants.ToolDefinition"]
+        tool_resources: Optional["Assistants.ToolResources"] = None
+        tool_choice: Optional["Assistants.ToolChoice"] = None
+
+    id: str
+    description: str | None = None
+    instructions: str | None = None
+    model: str
+    metadata: dict | None = None
+    name: str
+    temperature: float | None = None
+    top_p: float | None = None
+    tools: List["Assistants.ToolDefinition"]
+    tool_resources: Optional["Assistants.ToolResources"] = None
 
     @classmethod
     def create(
