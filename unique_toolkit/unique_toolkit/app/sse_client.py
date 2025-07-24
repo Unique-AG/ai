6ee6b14d59151e540cd0d1d7b676c7f917136c8e
klcd <cedric@unique.ch>
2025-07-24 15:56:15 +0200

    chore(ai): Add reusable settings, sse client and langchain utilities (#231)
    ## 🚀 Summary
    Refs: _link to or id of ticket_
    
    Briefly describe the changes introduced in this PR.
    
    ## ✅ Changes
    
    - [x] Added feature / fixed bug / updated docs
    - [ ] Refactored code / cleaned up
    - [ ] Other (describe below)
    
    ## 🧪 Testing
    
    Utility functions that do not need explicit testing
    
    ---------
    
    Co-authored-by: Cedric Klinkert <cedric.klinkert@six-group.com>

---
+++ unique_toolkit/unique_toolkit/app/sse_client.py
@@ -0,0 +1,22 @@
+from logging import getLogger
+
+from sseclient import SSEClient
+
+from unique_toolkit.app.unique_settings import UniqueSettings
+
+LOGGER = getLogger(__name__)
+
+
+def get_sse_client(
+    unique_settings: UniqueSettings,
+    subscriptions: list[str],
+) -> SSEClient:
+    url = f"{unique_settings.app.base_url}/public/event-socket/events/stream?subscriptions={','.join(subscriptions)}"
+    headers = {
+        "Authorization": f"Bearer {unique_settings.app.key}",
+        "x-app-id": unique_settings.app.id,
+        "x-company-id": unique_settings.auth.company_id,
+    }
+    LOGGER.info(f"SSEheaders: {headers}")
+    LOGGER.info(f"SSE Url: {url}")
+    return SSEClient(url=url, headers=headers)
---
+++ unique_toolkit/unique_toolkit/app/unique_settings.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+config_dict = SettingsConfigDict(
+    env_file_encoding="utf-8",
+    extra="ignore",
+)
+
+
+class UniqueApp(BaseSettings):
+    id: str
+    key: str
+    base_url: str
+    endpoint: str
+    endpoint_secret: str
+
+    model_config = SettingsConfigDict(env_prefix="unique_app_", **config_dict)
+
+
+class UniqueAuth(BaseSettings):
+    company_id: str
+    user_id: str
+
+    model_config = SettingsConfigDict(env_prefix="unique_auth_", **config_dict)
+
+
+class UniqueSettings:
+    app: UniqueApp = UniqueApp()
+    auth: UniqueAuth = UniqueAuth()
