276a35b663413ac3a84b440a4acb0bea63a3f573
Andreas Hauri <andreas@unique.ch>
2025-07-28 17:22:55 +0200

    feat(tool-kit): Add tool definition and Abstraction refactoring

--- unique_toolkit/unique_toolkit/tools/tool_factory.py
+++ unique_toolkit/unique_toolkit/tools/tool_factory.py
@@ -3,27 +3,29 @@ from typing import Callable
 from unique_toolkit.unique_toolkit.tools.tool_definitions import BaseToolConfig, Tool
 
 
-tool_map: dict[str, type[Tool]] = {}
-tool_config_map: dict[str, Callable] = {}
 
-   
-def register_tool(
-    cls,
-    tool: type[Tool],
-    tool_config: type[BaseToolConfig],
-):
-    cls.tool_map[tool.name] = tool
-    cls.tool_config_map[tool.name] = tool_config
+class ToolFactory:
+    tool_map: dict[str, type[Tool]] = {}
+    tool_config_map: dict[str, Callable] = {}
 
-   
-def build_tool(cls, tool_name: str, *args, **kwargs) -> Tool:
-    tool = cls.tool_map[tool_name](*args, **kwargs)
-    return tool
+    @classmethod
+    def register_tool(
+        cls,
+        tool: type[Tool],
+        tool_config: type[BaseToolConfig],
+    ):
+        cls.tool_map[tool.name] = tool
+        cls.tool_config_map[tool.name] = tool_config
 
+    @classmethod
+    def build_tool(cls, tool_name: str, *args, **kwargs) -> Tool:
+        tool = cls.tool_map[tool_name](*args, **kwargs)
+        return tool
 
-def build_tool_config(
-    cls, tool_name: str, **kwargs
-) -> BaseToolConfig:
-    if tool_name not in cls.tool_config_map:
-        raise ValueError(f"Tool {tool_name} not found")
-    return cls.tool_config_map[tool_name](**kwargs)
+    @classmethod
+    def build_tool_config(
+        cls, tool_name: str, **kwargs
+    ) -> BaseToolConfig:
+        if tool_name not in cls.tool_config_map:
+            raise ValueError(f"Tool {tool_name} not found")
+        return cls.tool_config_map[tool_name](**kwargs)
