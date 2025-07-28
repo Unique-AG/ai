34fb96196046deaaf217affe6908907bd89cf8e1
klcd <cedric@unique.ch>
2025-07-28 10:58:06 +0200

    fix(toolkit): Minor fixes settings sse client caused by SecretStr (#234)
    ## 🚀 Summary
    
    Fixes a minor issue with the pydantic settings using SecretStr
    
    ## ✅ Changes
    
    - [ ] Added feature / fixed bug / updated docs
    - [ ] Refactored code / cleaned up
    - [x] Other

--- unique_toolkit/unique_toolkit/app/sse_client.py
+++ unique_toolkit/unique_toolkit/app/sse_client.py
@@ -13,8 +13,8 @@ def get_sse_client(
 ) -> SSEClient:
     url = f"{unique_settings.app.base_url}/public/event-socket/events/stream?subscriptions={','.join(subscriptions)}"
     headers = {
-        "Authorization": f"Bearer {unique_settings.app.key}",
-        "x-app-id": unique_settings.app.id,
-        "x-company-id": unique_settings.auth.company_id,
+        "Authorization": f"Bearer {unique_settings.app.key.get_secret_value()}",
+        "x-app-id": unique_settings.app.id.get_secret_value(),
+        "x-company-id": unique_settings.auth.company_id.get_secret_value(),
     }
     return SSEClient(url=url, headers=headers)
