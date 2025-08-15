315671870160c16e9b807fa520b217ba0ababe98
Ahmed Jellouli <ahmed.jellouli.ext@unique.ch>
2025-08-15 14:08:02 +0200

    Reduce input token limits for claude_4 models

--- unique_toolkit/unique_toolkit/language_model/infos.py
+++ unique_toolkit/unique_toolkit/language_model/infos.py
@@ -547,7 +547,9 @@ class LanguageModelInfo(BaseModel):
                     version="claude-sonnet-4",
                     encoder_name=EncoderName.O200K_BASE,  # TODO: Update encoder with litellm
                     token_limits=LanguageModelTokenLimits(
-                        token_limit_input=200_000, token_limit_output=64_000
+                        # Input limit is 200_000, we leave 20_000 tokens as buffer due to tokenizer mismatch
+                        token_limit_input=180_000,
+                        token_limit_output=64_000,
                     ),
                     info_cutoff_at=date(2025, 3, 1),
                     published_at=date(2025, 5, 1),
@@ -565,7 +567,9 @@ class LanguageModelInfo(BaseModel):
                     version="claude-opus-4",
                     encoder_name=EncoderName.O200K_BASE,  # TODO: Update encoder with litellm
                     token_limits=LanguageModelTokenLimits(
-                        token_limit_input=200_000, token_limit_output=32_000
+                        # Input limit is 200_000, we leave 20_000 tokens as buffer due to tokenizer mismatch
+                        token_limit_input=180_000,
+                        token_limit_output=32_000,
                     ),
                     info_cutoff_at=date(2025, 3, 1),
                     published_at=date(2025, 5, 1),
