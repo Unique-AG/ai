b6697ce97aeeaa6f2d48b60da5161aefedf0c75d
Cedric Klinkert <cedric@unique.ch>
2025-08-30 13:50:23 +0200

    create separate init method instead

--- unique_toolkit/unique_toolkit/language_model/service.py
+++ unique_toolkit/unique_toolkit/language_model/service.py
@@ -91,20 +91,28 @@ class LanguageModelService:
         return cls(company_id=event.company_id, user_id=event.user_id)
 
     @classmethod
-    def from_settings(
-        cls, settings: UniqueSettings | None = None, env_filename: str = "unique.env"
-    ):
+    def from_settings(cls, settings: UniqueSettings):
         """
         Initialize the LanguageModelService with a settings object.
-        If the settings object is not provided, it will be initialized from the environment.
         """
-        if settings is None:
-            settings = UniqueSettings.from_env_auto_with_sdk_init(filename=env_filename)
-
         return cls(
             company_id=settings.auth.company_id.get_secret_value(),
             user_id=settings.auth.user_id.get_secret_value(),
         )
+
+    @classmethod
+    def from_settings_filename(
+        cls,
+        settings_filename: str = "unique.env",
+    ):
+        """
+        Initialize the LanguageModelService with automatically from a settings file placed in the
+        common plattform folders or the folder that the executed file is placed.
+        """
+        settings = UniqueSettings.from_env_auto_with_sdk_init(
+            filename=settings_filename
+        )
+        return cls.from_settings(settings=settings)
 
     @property
     @deprecated(
