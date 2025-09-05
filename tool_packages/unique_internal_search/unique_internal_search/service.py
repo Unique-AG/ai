0bae165287767564079a4daa0ad5e7361477d31d
Andreas Hauri <andreas@unique.ch>
2025-09-05 23:51:53 +0200

    fix

--- tool_packages/unique_internal_search/unique_internal_search/service.py
+++ tool_packages/unique_internal_search/unique_internal_search/service.py
@@ -1,4 +1,3 @@
-import copy
 from logging import Logger
 
 from pydantic import Field, create_model
@@ -100,9 +99,9 @@ class InternalSearchService:
         """
         if metadata_filter is None:
             metadata_filter = self.content_service._metadata_filter
-        #metadata_filter_copy = copy.deepcopy(self.content_service._metadata_filter)
+        # metadata_filter_copy = copy.deepcopy(self.content_service._metadata_filter)
         if chat_only and metadata_filter:
-            #self.content_service._metadata_filter = None
+            # self.content_service._metadata_filter = None
             metadata_filter = None
 
         try:
