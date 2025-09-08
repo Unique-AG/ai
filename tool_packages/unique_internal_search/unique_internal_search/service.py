cfff281dc761d8751e6601e04120cefff2ea2f81
Andreas Hauri <75168461+andreasunique@users.noreply.github.com>
2025-09-08 15:21:57 +0200

    fix(internal-search): Fix metadata filter bug in the lib (#339)
    ## 🚀 Summary
    
    
    ## ✅ Changes
    
    - [x] Added feature / fixed bug / updated docs

--- tool_packages/unique_internal_search/unique_internal_search/service.py
+++ tool_packages/unique_internal_search/unique_internal_search/service.py
@@ -99,9 +99,10 @@ class InternalSearchService:
         """
         if metadata_filter is None:
             metadata_filter = self.content_service._metadata_filter
-        # metadata_filter_copy = copy.deepcopy(self.content_service._metadata_filter)
+            metadata_filter_copy = self.content_service._metadata_filter
         if chat_only and metadata_filter:
-            # self.content_service._metadata_filter = None
+            # if this is not set to none search_content_chunks_async will overwrite it inside its call thats why it needs to stay.
+            self.content_service._metadata_filter = None
             metadata_filter = None
 
         try:
@@ -124,7 +125,7 @@ class InternalSearchService:
             raise e
 
         # Reset the metadata filter in case it was disabled
-        # self.content_service._metadata_filter = metadata_filter_copy
+        self.content_service._metadata_filter = metadata_filter_copy
 
         # Apply chunk relevancy sorter if enabled
         if self.config.chunk_relevancy_sort_config.enabled:
