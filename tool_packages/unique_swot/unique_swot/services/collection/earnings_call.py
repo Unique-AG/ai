dd83bf3255a7e21be907b67ecbabbb5c592c3414
ThePhilAz <rami.azouz@philico.com>
2025-10-07 15:48:04 +0200

    Change logging in main function

--- tool_packages/unique_swot/unique_swot/services/collection/earnings_call.py
+++ tool_packages/unique_swot/unique_swot/services/collection/earnings_call.py
@@ -1,5 +1,12 @@
+from logging import getLogger
+
 from unique_swot.services.schemas import Source
 
+logger = getLogger(__name__)
 
+# TODO: Implement a real earnings call collector
 def collect_earnings_calls() -> list[Source]:
+    logger.warning(
+        "Collecting earnings calls as a data source is not implemented yet. No sources will be collected."
+    )
     return []
--- tool_packages/unique_swot/unique_swot/services/collection/web.py
+++ tool_packages/unique_swot/unique_swot/services/collection/web.py
@@ -1,5 +1,12 @@
+from logging import getLogger
+
 from unique_swot.services.schemas import Source
 
+logger = getLogger(__name__)
 
+# TODO: Implement a real web source collector
 def collect_web_sources() -> list[Source]:
+    logger.warning(
+        "Collecting web sources as a data source is not implemented yet. No sources will be collected."
+    )
     return []
