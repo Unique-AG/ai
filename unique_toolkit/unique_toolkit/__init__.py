e23700e2e8b76034884ecd810f740a3e69e9ddd3
Cedric Klinkert <cedric@unique.ch>
2025-10-08 22:15:33 +0200

    optional import langchain

--- unique_toolkit/unique_toolkit/__init__.py
+++ unique_toolkit/unique_toolkit/__init__.py
@@ -2,7 +2,6 @@
 from unique_toolkit.chat import ChatService
 from unique_toolkit.content import ContentService
 from unique_toolkit.embedding import EmbeddingService
-from unique_toolkit.framework_utilities.langchain.client import get_langchain_client
 from unique_toolkit.framework_utilities.openai.client import (
     get_async_openai_client,
     get_openai_client,
@@ -15,6 +14,14 @@ from unique_toolkit.language_model import (
 )
 from unique_toolkit.services.knowledge_base import KnowledgeBaseService
 from unique_toolkit.short_term_memory import ShortTermMemoryService
+
+# Conditionally import langchain utilities if langchain is installed
+try:
+    from unique_toolkit.framework_utilities.langchain.client import get_langchain_client  # noqa: F401, I001
+
+    _LANGCHAIN_AVAILABLE = True
+except ImportError:
+    _LANGCHAIN_AVAILABLE = False
 
 # You can add other classes you frequently use here as well
 
@@ -30,5 +37,8 @@ __all__ = [
     "KnowledgeBaseService",
     "get_openai_client",
     "get_async_openai_client",
-    "get_langchain_client",
 ]
+
+# Add langchain-specific exports if available
+if _LANGCHAIN_AVAILABLE:
+    __all__.append("get_langchain_client")
