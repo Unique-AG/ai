d2be9a2b701dc4c2d2ee7847487cb70efb072de8
ThePhilAz <rami.azouz@philico.com>
2025-10-20 15:22:20 +0200

    Add Registry to track sources ids

--- tool_packages/unique_swot/unique_swot/services/collection/base.py
+++ tool_packages/unique_swot/unique_swot/services/collection/base.py
@@ -2,12 +2,14 @@ from logging import getLogger
 
 from pydantic import BaseModel, ConfigDict
 from unique_toolkit import KnowledgeBaseService
-from unique_toolkit.app.schemas import ChatEvent
 
-from unique_swot.services.collection.earnings_call import collect_earnings_calls
-from unique_swot.services.collection.knowledge_base import collect_knowledge_base
-from unique_swot.services.collection.web import collect_web_sources
-from unique_swot.services.schemas import Source
+from unique_swot.services.collection.registry import ContentChunkRegistry
+from unique_swot.services.collection.schema import Source
+from unique_swot.services.collection.sources import (
+    collect_earnings_calls,
+    collect_knowledge_base,
+    collect_web_sources,
+)
 
 _LOGGER = getLogger(__name__)
 
@@ -16,16 +18,7 @@ class CollectionContext(BaseModel):
     model_config = ConfigDict(frozen=True)
     use_earnings_calls: bool
     use_web_sources: bool
-
-    @classmethod
-    def from_event(cls, event: ChatEvent) -> "CollectionContext":
-        _LOGGER.warning(
-            "CollectionContext.from_event is not implemented yet. Defaulting to False for use_earnings_calls and use_web_sources."
-        )
-        return cls(
-            use_earnings_calls=False,
-            use_web_sources=False,
-        )
+    metadata_filter: dict | None
 
 
 class SourceCollectionManager:
@@ -34,29 +27,66 @@ class SourceCollectionManager:
         *,
         context: CollectionContext,
         knowledge_base_service: KnowledgeBaseService,
-        where_clause: dict,
+        content_chunk_registry: ContentChunkRegistry,
     ):
         self._context = context
         self._knowledge_base_service = knowledge_base_service
-        self._where_clause = where_clause
+        self._content_chunk_registry = content_chunk_registry
 
     def collect_sources(self) -> list[Source]:
-        sources = self.collect_internal_documents()
+        sources = self.collect_internal_documents(
+            metadata_filter=self._context.metadata_filter,
+            chunk_registry=self._content_chunk_registry,
+        )
 
-        if self._context.use_earnings_calls:
-            sources.extend(self.collect_earnings_calls())
-        if self._context.use_web_sources:
-            sources.extend(self.collect_web_sources())
+        sources.extend(
+            self.collect_earnings_calls(
+                use_earnings_calls=self._context.use_earnings_calls,
+                chunk_registry=self._content_chunk_registry,
+            )
+        )
+        sources.extend(
+            self.collect_web_sources(
+                use_web_sources=self._context.use_web_sources,
+                chunk_registry=self._content_chunk_registry,
+            )
+        )
+
+        # Save Registry Store in Memory Service
+        self._content_chunk_registry.save()
+
         return sources
 
-    def collect_earnings_calls(self) -> list[Source]:
+    def collect_earnings_calls(
+        self, *, use_earnings_calls: bool, chunk_registry: ContentChunkRegistry
+    ) -> list[Source]:
+        if not use_earnings_calls:
+            _LOGGER.warning("No earnings calls will be collected.")
+            return []
+        _LOGGER.info("Collecting earnings calls!")
         return collect_earnings_calls()
 
-    def collect_web_sources(self) -> list[Source]:
+    def collect_web_sources(
+        self, *, use_web_sources: bool, chunk_registry: ContentChunkRegistry
+    ) -> list[Source]:
+        if not use_web_sources:
+            _LOGGER.warning("No web sources will be collected.")
+            return []
+        _LOGGER.info("Collecting web sources!")
         return collect_web_sources()
 
-    def collect_internal_documents(self) -> list[Source]:
+    def collect_internal_documents(
+        self, *, metadata_filter: dict | None, chunk_registry: ContentChunkRegistry
+    ) -> list[Source]:
+        if metadata_filter is None:
+            _LOGGER.warning(
+                "No where clause provided. No internal documents will be collected."
+            )
+            return []
+        _LOGGER.info("Collecting internal documents!")
+        _LOGGER.info(f"Metadata filter: {metadata_filter}")
         return collect_knowledge_base(
             knowledge_base_service=self._knowledge_base_service,
-            where_clause=self._where_clause,
+            metadata_filter=metadata_filter,
+            chunk_registry=chunk_registry,
         )
---
+++ tool_packages/unique_swot/unique_swot/services/collection/sources/knowledge_base.py
@@ -0,0 +1,96 @@
+from logging import getLogger
+
+from unique_toolkit import KnowledgeBaseService
+from unique_toolkit.content import ContentChunk
+from unique_toolkit.content.schemas import (
+    ContentInfo,
+)
+
+from unique_swot.services.collection.registry import ContentChunkRegistry
+from unique_swot.services.collection.schema import Source, SourceChunk, SourceType
+
+_LOGGER = getLogger(__name__)
+
+
+def collect_knowledge_base(
+    *,
+    knowledge_base_service: KnowledgeBaseService,
+    metadata_filter: dict,
+    chunk_registry: ContentChunkRegistry,
+) -> list[Source]:
+    contents = knowledge_base_service.get_paginated_content_infos(
+        metadata_filter=metadata_filter
+    )
+    sources = _convert_content_to_sources(
+        knowledge_base_service=knowledge_base_service,
+        content_infos=contents.content_infos,
+        chunk_registry=chunk_registry,
+    )
+
+    return sources
+
+
+def _convert_content_to_sources(
+    *,
+    knowledge_base_service: KnowledgeBaseService,
+    content_infos: list[ContentInfo],
+    chunk_registry: ContentChunkRegistry,
+) -> list[Source]:
+    sources = []
+    sources_with_no_chunks = []
+    for content_info in content_infos:
+        title = content_info.title or content_info.key or "Unknown Title"
+        url = content_info.url
+        chunks = _get_chunks_from_content(
+            knowledge_base_service=knowledge_base_service,
+            content_id=content_info.id,
+            chunk_registry=chunk_registry,
+        )
+        if len(chunks) == 0:
+            sources_with_no_chunks.append(title)
+            continue
+
+        sources.append(
+            Source(
+                type=SourceType.INTERNAL_DOCUMENT,
+                url=url,
+                title=title,
+                chunks=chunks,
+            )
+        )
+
+    if len(sources_with_no_chunks) > 0:
+        no_chunks_str = "\n- ".join(sources_with_no_chunks)
+        _LOGGER.warning(
+            f"The following internal documents have no chunks: \n- {no_chunks_str}"
+        )
+
+    return sources
+
+
+def _get_chunks_from_content(
+    *,
+    knowledge_base_service: KnowledgeBaseService,
+    content_id: str,
+    chunk_registry: ContentChunkRegistry,
+) -> list[SourceChunk]:
+    contents = knowledge_base_service.search_contents(
+        where={"id": {"equals": content_id}}
+    )
+
+    assert len(contents) == 1, (
+        "Expected exactly one content to be found for the given content id"
+    )
+
+    chunks = []
+    for chunk in sorted(contents[0].chunks, key=lambda x: x.order):
+        cleaned_chunk = _clean_up_content_chunk(chunk)
+        chunk_id = chunk_registry.add(cleaned_chunk)
+        chunks.append(SourceChunk(id=chunk_id, text=chunk.text))
+
+    return chunks
+
+
+def _clean_up_content_chunk(content_chunk: ContentChunk) -> ContentChunk:
+    content_chunk.text = ""
+    return content_chunk
