78c7ae0d7bc68286cc32799fbc47287d505e970c
ThePhilAz <rami.azouz@philico.com>
2025-10-23 10:19:33 +0200

    remove old comments

--- tool_packages/unique_swot/unique_swot/services/notifier.py
+++ tool_packages/unique_swot/unique_swot/services/notifier.py
@@ -5,11 +5,11 @@ from typing import Any
 from pydantic import BaseModel
 from unique_toolkit import ChatService
 from unique_toolkit.chat.schemas import (
-    # MessageExecutionStatus,
+    MessageExecutionStatus,
     MessageExecutionType,
-    # MessageExecutionUpdateStatus,
-    # MessageLogDetails,
-    # MessageLogEvent,
+    MessageExecutionUpdateStatus,
+    MessageLogDetails,
+    MessageLogEvent,
     MessageLogStatus,
 )
 
@@ -33,81 +33,48 @@ class Notifier(ABC):
 
 
 class ProgressNotifier(Notifier):
-    def __init__(self, chat_service: ChatService, message_id: str, total_steps: int):
+    def __init__(self, chat_service: ChatService, message_id: str):
         self._chat_service = chat_service
-        self._total_steps = total_steps
         self._processed_steps = 0
+        self._execution_registery = {}
+        
+    @property
+    def progress_precentage(self):
+        return _calculate_percentage_completed(self._processed_steps, self._total_steps)
+    
+    
+    def start_progress(self, total_steps: int):
+        self._total_steps = total_steps
+        
         self._chat_service.create_message_execution(
             message_id=self._chat_service._assistant_message_id,
             type=MessageExecutionType.DEEP_RESEARCH,
             percentage_completed=0,
         )
+        
+    def notify(self, step_name: str, progress: float):
+        self._chat_service.create_message_log(
+            message_id=self._chat_service._assistant_message_id,
+            text=f"{step_name}: {progress}",
+            status=MessageLogStatus.RUNNING,
+            order=0,
+            details=MessageLogDetails(
+                data=[
+                    MessageLogEvent(
+                        type="InternalSearch",
+                        text=f"{step_name}: {progress}",
+                    )
+                ]
+            ),
+        )
 
-        self._execution_registery = {}
 
-    @property
-    def progress_precentage(self):
-        return _calculate_percentage_completed(self._processed_steps, self._total_steps)
-
-    # def notify(self, step_id, status: MessageLogStatus, details: str) -> None:
-    #     self._execution_registery[step_id] = StepRegistry(
-    #         status=status, details=details
-    #     )
-
-    #     self._chat_service.create_message_log(
-    #         message_id=self._chat_service._assistant_message_id,
-    #         text=f"Step {current_step} of {self._total_steps} completed",
-    #         status=MessageLogStatus.RUNNING,
-    #         order=current_step,
-    #         details=MessageLogDetails(
-    #             data=[
-    #                 MessageLogEvent(
-    #                     type="SWOTAnalysis",
-    #                     text=details,
-    #                 )
-    #             ]
-    #         ),
-    #     )
-
-    #     self._chat_service.update_message_execution(
-    #         message_id=self._chat_service._assistant_message_id,
-    #         status=MessageExecutionUpdateStatus.RUNNING,
-    #         percentage_completed=percentage_completed,
-    #     )
-
-    # def _create_message_log(
-    #     self, step_id: str, status: MessageLogStatus, details: str
-    # ) -> None:
-    #     message_log = self._chat_service.create_message_log(
-    #         message_id=self._chat_service._assistant_message_id,
-    #         text=f"Step {current_step} of {self._total_steps} completed",
-    #         status=MessageLogStatus.RUNNING,
-    #         order=current_step,
-    #         details=MessageLogDetails(
-    #             data=[
-    #                 MessageLogEvent(
-    #                     type="SWOTAnalysis",
-    #                     text=details,
-    #                 )
-    #             ]
-    #         ),
-    #     )
-    #     return StepRegistry(
-    #         message_log_id=message_log.message_log_id, status=status, details=details
-    #     )
-
-    # def _update_message_execution(
-    #     self,
-    #     step_id: str,
-    #     status: MessageExecutionUpdateStatus,
-    #     percentage_completed: int,
-    # ) -> None:
-    #     self._chat_service.update_message_log(
-    #         message_id=self._chat_service._assistant_message_id,
-    #         status=status,
-    #         percentage_completed=percentage_completed,
-    #     )
-
+    def end_progress(self):
+        self._chat_service.update_message_execution(
+            message_id=self._chat_service._assistant_message_id,
+            status=MessageExecutionUpdateStatus.COMPLETED,
+            percentage_completed=100,
+        )
 
 class LoggerNotifier(Notifier):
     def notify(self, step_name: str, progress: float) -> None:
