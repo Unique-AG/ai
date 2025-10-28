ced05fd65f438a51589c0c49559f20a46d825b25
lucio-unique <lucio@unique.ch>
2025-10-28 22:20:39 +1100

    fixed chat_id None Type inconsistencies

--- tool_packages/unique_internal_search/unique_internal_search/service.py
+++ tool_packages/unique_internal_search/unique_internal_search/service.py
@@ -39,7 +39,7 @@ class InternalSearchService:
         config: InternalSearchConfig,
         content_service: ContentService,
         chunk_relevancy_sorter: ChunkRelevancySorter,
-        chat_id: str,
+        chat_id: str | None,
         logger: Logger,
     ):
         self.config = config
@@ -139,7 +139,7 @@ class InternalSearchService:
                     search_language=self.config.search_language,
                     scope_ids=self.config.scope_ids,
                     metadata_filter=metadata_filter,
-                    chat_id=self.chat_id if self.config.exclude_uploaded_files else "",
+                    chat_id=self.chat_id if self.config.exclude_uploaded_files and self.chat_id else "",
                     chat_only=chat_only,
                     content_ids=content_ids,
                     score_threshold=self.config.score_threshold,
