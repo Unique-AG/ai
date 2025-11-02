457981993376494d6c47f949e78d0efbc3e314f1
Cedric Klinkert <cedric@unique.ch>
2025-11-02 22:49:45 +0100

    more example openai

--- unique_toolkit/docs/.python_files/openai_responses_function_call.py
+++ unique_toolkit/docs/.python_files/openai_responses_function_call.py
@@ -1,4 +1,4 @@
-# ~/~ begin <<docs/plattforms/openai/openai.md#docs/.python_files/openai_completions.py>>[init]
+# ~/~ begin <<docs/plattforms/openai/openai.md#docs/.python_files/openai_responses_function_call.py>>[init]
 # ~/~ begin <<docs/setup/_common_imports.md#common_imports>>[init]
 from unique_toolkit.app.unique_settings import UniqueSettings
 from unique_toolkit.app.init_sdk import init_unique_sdk
@@ -33,40 +33,7 @@ model = LanguageModelName.AZURE_GPT_4o_2024_1120
 # ~/~ begin <<docs/plattforms/openai/openai.md#get_openai_client>>[init]
 client = get_openai_client()
 # ~/~ end
-# ~/~ begin <<docs/plattforms/openai/openai.md#openai_chat_completion_messages>>[init]
-
-messages = (
-    OpenAIMessageBuilder()
-    .system_message_append(content="You are a helpful assistant")
-    .user_message_append(content="How is the weather in New York")
-).messages
-# ~/~ end
-# ~/~ begin <<docs/plattforms/openai/openai.md#openai_chat_completion_simple_completion>>[init]
-# Simple Completion
-response = client.chat.completions.create(
-    messages=messages,
-    model=model,
-)
-for c in response:
-    print(c)
-# ~/~ end
-# ~/~ begin <<docs/plattforms/openai/openai.md#openai_chat_completion_structured_output>>[init]
-
-
-class CalendarEvent(BaseModel):
-    name: str
-    date: str
-    participants: list[str]
-
-
-completion = client.beta.chat.completions.parse(
-    model=model,
-    messages=messages,
-    response_format=CalendarEvent,
-)
-completion.choices[0].message.content
-# ~/~ end
-# ~/~ begin <<docs/plattforms/openai/openai.md#openai_chat_completion_function_calling>>[init]
+# ~/~ begin <<docs/plattforms/openai/openai.md#openai_tool_description>>[init]
 from pydantic import Field
 from unique_toolkit import LanguageModelToolDescription
 
@@ -82,33 +49,21 @@ weather_tool_description = LanguageModelToolDescription(
 )
 
 weather_tool_description_toolkit= weather_tool_description.to_openai()
-
+# ~/~ end
+# ~/~ begin <<docs/plattforms/openai/openai.md#openai_responses_function_calling>>[init]
 messages = (
     OpenAIMessageBuilder()
     .system_message_append(content="You are a helpful assistant")
     .user_message_append(content="What is the weather like in Paris today?")
 ).messages
 
-completion = client.chat.completions.create(
+weather_tool_description_toolkit = weather_tool_description.to_openai(mode="responses")
+
+response = client.responses.create(
     model=model,
-    messages=messages,
-    tools=[weather_tool_description_toolkit],
+    tools=[weather_tool_description_toolkit], # type: ignore
+    input=messages, # type: ignore
+
 )
-
-completion.choices[0].message.tool_calls
-# ~/~ end
-# ~/~ begin <<docs/plattforms/openai/openai.md#openai_chat_completion_developer_message>>[init]
-
-messages = (
-    OpenAIMessageBuilder()
-    .system_message_append(content="You are a helpful assistant")
-    .user_message_append(content="How is the weather in New York")
-).messages
-
-completion = client.chat.completions.create(
-    model=model,
-    messages=messages,
-)
-print(completion.choices[0].message.content)
 # ~/~ end
 # ~/~ end
