28444b53ee4dcfa78a792fe650ba8ab4d6f480eb
irina-unique <irina@unique.ch>
2025-11-19 17:01:56 +0100

    feat(sdk): add expired/s at fields [UN-15051]

--- unique_sdk/unique_sdk/api_resources/_content.py
+++ unique_sdk/unique_sdk/api_resources/_content.py
@@ -29,6 +29,8 @@ class Content(APIResource["Content"]):
     metadata: Optional[Dict[str, Any]]
     writeUrl: Optional[str]
     readUrl: Optional[str]
+    expiresAt: Optional[str]
+    expiredAt: Optional[str]
 
     class QueryMode(Enum):
         Default = "default"
