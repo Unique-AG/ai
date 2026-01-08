2ab0bbc931485b0dbf7f0719aa8a09c0473d8909
Azeez Raheem <raheemazeez4@gmil.com>
2026-01-08 08:58:00 +0000

    fix(unique_six): Remove missing function

--- connectors/unique_six/unique_six/__init__.py
+++ connectors/unique_six/unique_six/__init__.py
@@ -1,17 +1,13 @@
 from unique_six.client import (
     SixApiClient,
-    get_six_api_client,
 )
 from unique_six.exception import (
-    NoCredentialsException,
     SixApiException,
     raise_errors_from_api_response,
 )
 
 __all__ = [
     "SixApiClient",
-    "get_six_api_client",
     "SixApiException",
     "raise_errors_from_api_response",
-    "NoCredentialsException",
 ]
