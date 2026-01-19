cec2e02dcb5da25b1a18c9d524a752f258d02ee6
Azeez Raheem <raheemazeez4@gmil.com>
2026-01-19 08:33:41 +0000

    Consolidate file utilities

--- unique_toolkit/unique_toolkit/_common/utils/files.py
+++ unique_toolkit/unique_toolkit/_common/utils/files.py
@@ -1,5 +1,6 @@
 import mimetypes
 from enum import StrEnum
+from pathlib import Path
 
 
 class FileMimeType(StrEnum):
@@ -8,11 +9,70 @@ class FileMimeType(StrEnum):
     DOC = "application/msword"
     XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     XLS = "application/vnd.ms-excel"
+    PPT = "application/vnd.ms-powerpoint"
     PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
     CSV = "text/csv"
     HTML = "text/html"
     MD = "text/markdown"
     TXT = "text/plain"
+    EXCEL = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    JSON = "application/json"
+
+
+def get_common_name(extension: FileMimeType) -> str:
+    match extension:
+        case FileMimeType.DOCX:
+            return "docx"
+        case FileMimeType.XLSX | FileMimeType.XLS:
+            return "excel"
+        case FileMimeType.PPT | FileMimeType.PPTX:
+            return "powerpoint"
+        case FileMimeType.PDF:
+            return "pdf"
+        case FileMimeType.JSON:
+            return "json"
+        case FileMimeType.CSV:
+            return "csv"
+        case _:
+            return "unknown"
+
+
+def is_docx_mime(filepath: Path) -> bool:
+    mime_type, _ = mimetypes.guess_type(filepath)
+    return mime_type == FileMimeType.DOCX
+
+
+def is_pdf_mime(filepath: Path) -> bool:
+    mime_type, _ = mimetypes.guess_type(filepath)
+    return mime_type == FileMimeType.PDF
+
+
+def is_xlsx_mime(filepath: Path) -> bool:
+    mime_type, _ = mimetypes.guess_type(filepath)
+    return mime_type in [FileMimeType.XLSX, FileMimeType.XLSX]
+
+
+def is_pptx_mime(filepath: Path) -> bool:
+    mime_type, _ = mimetypes.guess_type(filepath)
+    return mime_type in [FileMimeType.PPTX, FileMimeType.PPTX]
+
+
+def is_json_mime(filepath: Path) -> bool:
+    mime_type, _ = mimetypes.guess_type(filepath)
+    return mime_type == FileMimeType.JSON
+
+
+
+def is_valid_mime(
+        filepath: Path, valid_mimes: list[FileMimeType]
+) -> bool:
+    mime_type, _ = mimetypes.guess_type(filepath)
+    return mime_type in valid_mimes
+
+
+def get_file_extensions(mimes: list[FileMimeType]) -> list[str]:
+    types = [mimetypes.guess_extension(mime) for mime in mimes]
+    return [t for t in types if t]
 
 
 class ImageMimeType(StrEnum):
