96e55959749ede1c4742693c6394c62abbfcf620
Tin Oroz <tin.oroz@unique.ai>
2026-02-05 11:08:24 +0100

    Updating chat utils usage of count tokens

--- unique_toolkit/unique_toolkit/chat/utils.py
+++ unique_toolkit/unique_toolkit/chat/utils.py
@@ -1,15 +1,18 @@
+from unique_toolkit._common.token import count_tokens
 from unique_toolkit.chat.schemas import ChatMessage
-from unique_toolkit.content.utils import count_tokens
+from unique_toolkit.language_model.infos import LanguageModelInfo
 
 
 def convert_chat_history_to_injectable_string(
     history: list[ChatMessage],
+    model_info: LanguageModelInfo | None = None,
 ) -> tuple[list[str], int]:
     """
     Converts chat history to a string that can be injected into the model.
 
     Args:
         history (list[ChatMessage]): The chat history.
+        model_info (LanguageModelInfo | None): The language model to use for token counting.
 
     Returns:
         tuple[list[str], int]: The chat history and the token length of the chat context.
@@ -21,5 +24,5 @@ def convert_chat_history_to_injectable_string(
         else:
             chatHistory.append(f"previous_question: {msg.content}")
     chatContext = "\n".join(chatHistory)
-    chatContextTokenLength = count_tokens(chatContext)
+    chatContextTokenLength = count_tokens(chatContext, model=model_info)
     return chatHistory, chatContextTokenLength
