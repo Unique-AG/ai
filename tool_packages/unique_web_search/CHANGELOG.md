ad6bb5821075d8f2811cc55e10f255df2575c514
ThePhilAz <rami.azouz@philico.com>
2026-02-16 17:10:36 +0100

    fix(web-search): Message Log improvements

--- tool_packages/unique_web_search/CHANGELOG.md
+++ tool_packages/unique_web_search/CHANGELOG.md
@@ -5,6 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), 
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.11.4] - 2026-02-16
+- Messagelog alignment
+
 ## [1.11.3] - 2026-02-13
 - method `_create_agent_run_with_agent_id` in `runner.py` shouldn't use model from settings
 
--- tool_packages/unique_web_search/src/unique_web_search/services/message_log.py
+++ tool_packages/unique_web_search/src/unique_web_search/services/message_log.py
@@ -22,12 +22,12 @@ class WebSearchMessageLogger:
         self._progress_message = ""
 
     async def finished(self) -> None:
-        self._progress_message = "Search completed!"
+        self._progress_message = ""
         self._status = MessageLogStatus.COMPLETED
         await self._propagate_message_log()
 
     async def failed(self) -> None:
-        self._progress_message = "Search failed!"
+        self._progress_message = "_Search failed_"
         self._status = MessageLogStatus.FAILED
         await self._propagate_message_log()
 
