353b500f4657d231d7837b2b088498fb8e4b63a2
Sanzio Monti <github@sanziomonti.xyz>
2026-03-04 12:16:11 +0100

    refactor(unique_stock_ticker): move get_six_api_client to a new client module
    This change relocates the `get_six_api_client` function from the `__init__.py` file to a newly created `client.py` module, streamlining the import structure and enhancing code organization. The `__all__` variable is updated accordingly to reflect the new structure.

--- postprocessors/unique_stock_ticker/unique_stock_ticker/clients/six/__init__.py
+++ postprocessors/unique_stock_ticker/unique_stock_ticker/clients/six/__init__.py
@@ -1,21 +1,3 @@
-from unique_six import SixApiClient, SixApiException, raise_errors_from_api_response
+from unique_stock_ticker.clients.six.client import get_six_api_client
 
-from unique_stock_ticker.clients.six.exception import NoCredentialsException
-
-
-def get_six_api_client(company_id: str) -> SixApiClient:
-    from unique_stock_ticker.clients.six.settings import six_api_settings
-
-    creds = six_api_settings.creds_for_company(company_id)
-    if creds is None:
-        raise NoCredentialsException(company_id)
-    return SixApiClient(creds.cert, creds.key)
-
-
-__all__ = [
-    "SixApiClient",
-    "get_six_api_client",
-    "SixApiException",
-    "raise_errors_from_api_response",
-    "NoCredentialsException",
-]
+__all__ = ["get_six_api_client"]
