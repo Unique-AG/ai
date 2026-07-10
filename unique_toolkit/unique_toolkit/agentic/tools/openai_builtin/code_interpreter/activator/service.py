f34783eb44c7967363fede276cf2d862d5001c11
Ahmed Jellouli <ahmed.jellouli@philico.com>
2026-07-10 14:02:14 +0200

    Remove duplicated prompt

--- unique_toolkit/unique_toolkit/agentic/tools/openai_builtin/code_interpreter/activator/service.py
+++ unique_toolkit/unique_toolkit/agentic/tools/openai_builtin/code_interpreter/activator/service.py
@@ -30,16 +30,6 @@ class _CodeParams(BaseModel):
     arg: bool = Field(True, description="Ignored Argument.")
 
 
-_DEFAULT_TOOL_DESCRIPTION = """
-Use this tool in order to activate the code interpreter tool in this environment, which allows you to execute python code in a secure environment.
-Do NOT mention to the user that you need to activate the python tool, always simply do it!
-After activation you will get more instructions on how to use the python tool.
-Again: Assume you HAVE access to the python tool! just activate it.
-""".strip()
-
-_DEFAULT_TOOL_DESCRIPTION_FOR_SYSTEM_PROMPT = _DEFAULT_TOOL_DESCRIPTION
-
-
 class CodeInterpreterActivatorTool(Tool[CodeInterpreterActivatorConfig]):
     NAME = "ActivatePython"
     DISPLAY_NAME = "Activate Python"
